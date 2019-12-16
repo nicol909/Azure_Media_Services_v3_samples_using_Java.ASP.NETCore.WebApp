71c7eb50c6d8cec98081e39ada1db869cf75e935
Nico Leputois <nicoleputois@outlook.com>
2019-12-16 22:11:30 +0100

    Ajoutez des fichiers projet.

---
+++ Azure_Media_Services_v3_samples_using_Java.ASP.NETCore.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Azure_Media_Services_v3_samples_using_Java.ASP.NETCore.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
